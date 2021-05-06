b0d8702957dbeeb1a2176554a746fc1a779216da
Kaktushose <faulie50@gmail.com>
2021-05-06 17:42:48 +0200

    update database model

--- src/main/java/de/bluelight/api/database/model/User.java
+++ src/main/java/de/bluelight/api/database/model/User.java
@@ -1,6 +1,7 @@
 package de.bluelight.api.database.model;
 
 import javax.persistence.*;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -12,16 +13,23 @@ public class User {
     private String name;
     private String email;
     private String password;
-
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private List<Station> stations;
+    @OneToMany(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @JoinColumn(name = "userId", referencedColumnName = "userId")
+    private List<Mission> missions;
     public User() {
 
     }
 
-    public User(long userId, String name, String email, String password) {
+    public User(long userId, String name, String email, String password, List<Station> stations, List<Mission> missions) {
         this.userId = userId;
         this.name = name;
         this.email = email;
         this.password = password;
+        this.stations = stations;
+        this.missions = missions;
     }
 
     public long getUserId() {
@@ -54,5 +62,21 @@ public class User {
 
     public void setPassword(String password) {
         this.password = password;
+    }
+
+    public List<Station> getStations() {
+        return stations;
+    }
+
+    public void setStations(List<Station> stations) {
+        this.stations = stations;
+    }
+
+    public List<Mission> getMissions() {
+        return missions;
+    }
+
+    public void setMissions(List<Mission> missions) {
+        this.missions = missions;
     }
 }
